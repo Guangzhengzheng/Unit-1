76b96fceaf175b94c8df1b6ccd6618c25cfc7c68
Guangzhengzheng <819380@mystma.org>
2020-09-14 15:40:24 +0000

    Added Top Five (for this list) Movies

--- unit 1/src/app/App.java
+++ unit 1/src/app/App.java
@@ -2,6 +2,10 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("1. Garfield: the Movie");
+        System.out.println("2. Garfield's Pet Force");
+        System.out.println("3. Furry Veangance");
+        System.out.println("4. G-Force");
+        System.out.println("5. Thomas & Friends: King of the Railway");
     }
 }
