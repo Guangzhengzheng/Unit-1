abccf3d1586499f54185416994b59250b832e95c
Guangzhengzheng <819380@mystma.org>
2020-09-18 15:30:49 +0000

    Variables of Data

--- unit 1/src/app/App.java
+++ unit 1/src/app/App.java
@@ -2,10 +2,44 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1. Garfield: the Movie");
-        System.out.println("2. Garfield's Pet Force");
-        System.out.println("3. Furry Veangance");
-        System.out.println("4. G-Force");
-        System.out.println("5. Thomas & Friends: King of the Railway");
-    }
+        boolean livesOnSodor = false;
+        
+        System.out.println("Do I live on Sodor");
+        System.out.println(livesOnSodor);
+        
+        byte octopusArms = 8;
+
+        System.out.println("Do many arms does an octopus have?");
+        System.out.println(octopusArms);
+
+        char firstLetterOfName = 'T';
+
+        System.out.println("The first letter of my name is:");
+        System.out.println(firstLetterOfName);
+
+        short stmaPop = 25397;
+        
+        System.out.println("How many people live in St. Michael, Albertville");
+        System.out.println(stmaPop);
+
+        int myFavNum = 98462;
+
+         System.out.println(myFavNum);
+
+        long worldPop = 594000000;
+
+        System.out.println("What is the world population?");
+        System.out.println(worldPop);
+
+        float pi = 3.1415926f;
+
+        System.out.println("What is pi to 7 decimal points?");
+        System.out.println(pi);
+
+        double longPi = 3.14159265359;
+
+        System.out.println("What is pi to 11 decimal points?");
+        System.out.println(longPi);
+
+        }
 }
