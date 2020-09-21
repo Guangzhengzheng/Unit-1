4d34b05d14ba4c1b2b3c80fb54ee7578fbb22c31
Guangzhengzheng <819380@mystma.org>
2020-09-21 14:46:50 +0000

    String code

--- unit 1/src/app/App.java
+++ unit 1/src/app/App.java
@@ -2,44 +2,14 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        boolean livesOnSodor = false;
-        
-        System.out.println("Do I live on Sodor");
-        System.out.println(livesOnSodor);
-        
-        byte octopusArms = 8;
+        String myName = "Hector";
 
-        System.out.println("Do many arms does an octopus have?");
-        System.out.println(octopusArms);
+        String firstName = "Hector";
+        String lastName = "Zheng";
 
-        char firstLetterOfName = 'T';
+        int int1 = 3;
+        int int2 = 4;
 
-        System.out.println("The first letter of my name is:");
-        System.out.println(firstLetterOfName);
-
-        short stmaPop = 25397;
-        
-        System.out.println("How many people live in St. Michael, Albertville");
-        System.out.println(stmaPop);
-
-        int myFavNum = 98462;
-
-         System.out.println(myFavNum);
-
-        long worldPop = 594000000;
-
-        System.out.println("What is the world population?");
-        System.out.println(worldPop);
-
-        float pi = 3.1415926f;
-
-        System.out.println("What is pi to 7 decimal points?");
-        System.out.println(pi);
-
-        double longPi = 3.14159265359;
-
-        System.out.println("What is pi to 11 decimal points?");
-        System.out.println(longPi);
-
+        System.out.println(myName + " " + lastName + " " + int1 + int2);
         }
 }
