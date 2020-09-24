bd0f44cfa5b632984a5ef565b9f75822d4af762b
Guangzhengzheng <819380@mystma.org>
2020-09-24 16:14:07 +0000

    Java Scanner

--- unit 1/src/app/App.java
+++ unit 1/src/app/App.java
@@ -1,22 +1,23 @@
 package app;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String person1 = "Casey";
-        String person2 = "Mack";
-        String verb1 = "carry";
-        String verb2 = "fix";
-        String verb3 = "search";
-        String dayOfWeek = "Monday";
-        String food = "cookies";
-        String place2 = "shed";
-        String place1 = "home";
-        String adjective1 = "crazy";
-        String adjective2 = "robotic";
 
-        System.out.println(person1 + " and " + person2 + " are 2 kids with big dreams in their " + place1 + "town of Ffarqhar on Sodor. The two of them rarely do things independently.");
-        System.out.println("Every " + dayOfWeek + ", the 2 go to a " + place2 + " to work on their various projects, which are deemed " + adjective1 + " " + adjective2 + "s by the rest of the townsfolk.");  
-        System.out.println("Whenever one of them finds a flaw, the two "+ verb1 + " out the repair processes, " + verb3 + "ing for anything on their project that might need to be "+ verb2 + "ed.");  
-        System.out.println("After a long day, however, they enjoy snacking on "+ food + " after successful operations on their projects.");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter your first name.");
+        String firstName = input.nextLine();
+        System.out.println("Please enter your last name.");
+        String lastName = input.nextLine();
+
+        System.out.println("Please enter your age.");
+        String age = input.nextLine();
+        System.out.println("Please enter your current grade in school.");
+        String grade = input.nextLine();
+        System.out.println("Please enter your favorite color.");
+        String favoriteColor = input.nextLine();
+        
+        System.out.println("Hello, " + firstName + " " + lastName + ". You are " + age + " years old, currently in " + grade + " grade, and your favorite color is " + favoriteColor + " of them.");
     }
 }
