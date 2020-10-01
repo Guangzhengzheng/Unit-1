114213ff811c158171f900e20b8156cf852b9d20
Guangzhengzheng <819380@mystma.org>
2020-10-01 17:42:14 +0000

    For loops completed

--- unit 1/src/app/App.java
+++ unit 1/src/app/App.java
@@ -1,26 +1,39 @@
 package app;
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Scanner input = new Scanner(System.in);
+        for (int i = 0; i <= 10; i = i + 1){
+            System.out.println("The count is " + i);
+            System.out.println("Let's keep counting.");
+        }
 
-        System.out.println("Please enter a color");
-        String color = input.nextLine();
+        for (int i = 10; i <= 20; i = i + 1){
+            System.out.println("The count is " + i);
+            System.out.println("Let's keep counting.");
+        }
 
-        System.out.println("Please enter a number");
-        int number = input.nextInt();
+        for (int i = 10; i <= 100; i = i + 10){
+            System.out.println("The count is " + i);
+            System.out.println("Let's keep counting.");
+        }
 
-        int count = 0;
+        for (int i = 1000; i <= 256000; i = i + 1000){
+            System.out.println("The count is " + i);
+            System.out.println("Let's keep counting.");
+        }
 
-        while (count < number) {
-            System.out.println(color);
-            count = count + 1;
-            }
+        for (int i = 11; i <= 12; i = i + 1){
+            System.out.println("The count is " + i);
+            System.out.println("Let's keep counting.");
+        }
+
+        for (int i = 10; i >= 0; i = i - 1){
+            System.out.println("The count is " + i);
+            System.out.println("Let's keep counting.");
         }
 
 
-
-
+    
+    }
 }
