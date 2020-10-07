f33ef4373808ca513440819f79c20b566d61bed8
Guangzhengzheng <819380@mystma.org>
2020-10-07 18:56:33 +0000

    if color quiz

--- unit 1/src/app/App.java
+++ unit 1/src/app/App.java
@@ -1,16 +1,72 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        for (int i = 1; i <= 20; i++) {
-            if (i % 4 == 0) {
-                System.out.println(i + " is able to be divided evenly by 4");
-            } else {
-                System.out.println(i + " is unable to be divided evenly by 4");
-            }
+
+String question1 = "what is the color of a banana?";
+String cAnswer1 = "yellow";
+String question2 = "what is the color of an apple?";
+String cAnswer2 = "red";
+String question3 = "what is the color of the sky?";
+String cAnswer3 = "blue";
+String question4 = "what is the color of snow?";
+String cAnswer4 = "white";
+String question5 = "what is the color of grass?";
+String cAnswer5 = "green";
+int correctAnswer = 0;
+int percentage = 0;
+Scanner input = new Scanner(System.in);
+
+
+    System.out.println(question1);
+     String answer1 = input.nextLine();
+        if (answer1.equals(cAnswer1)) {
+            System.out.println("Correct"); 
+            correctAnswer++;
+        } else {
+            System.out.println("Incorrect");
         }
 
-        System.out.println("All finished");
+     System.out.println(question2);
+     String answer2 = input.nextLine();
+        if (answer2.equals(cAnswer2)) {
+            System.out.println("Correct"); 
+            correctAnswer++;
+        } else {
+            System.out.println("Incorrect");
+        }
+
+     System.out.println(question3);
+     String answer3 = input.nextLine();
+        if (answer3.equals(cAnswer3)) {
+            System.out.println("Correct"); 
+            correctAnswer++;
+        } else {
+            System.out.println("Incorrect");
+        }
+
+     System.out.println(question4);
+     String answer4 = input.nextLine();
+        if (answer4.equals(cAnswer4)) {
+            System.out.println("Correct"); 
+            correctAnswer++;
+        } else {
+            System.out.println("Incorrect");
+        }
+
+     System.out.println(question5);
+     String answer5 = input.nextLine();
+        if (answer5.equals(cAnswer5)) {
+            System.out.println("Correct"); 
+            correctAnswer++;
+        } else {
+            System.out.println("Incorrect");
+        }
+
+    percentage = correctAnswer * 20;
+    System.out.println("you have "+ correctAnswer +" out of 5 correct " + percentage + "%" );
     }
+    
 }
