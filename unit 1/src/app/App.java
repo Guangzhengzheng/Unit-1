cc4bdfd9845b6e4743853033862417defe665697
Guangzhengzheng <819380@mystma.org>
2020-10-09 14:08:56 +0000

    Otter code

--- unit 1/src/app/App.java
+++ unit 1/src/app/App.java
@@ -5,68 +5,33 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
-String question1 = "what is the color of a banana?";
-String cAnswer1 = "yellow";
-String question2 = "what is the color of an apple?";
-String cAnswer2 = "red";
-String question3 = "what is the color of the sky?";
-String cAnswer3 = "blue";
-String question4 = "what is the color of snow?";
-String cAnswer4 = "white";
-String question5 = "what is the color of grass?";
-String cAnswer5 = "green";
-int correctAnswer = 0;
-int percentage = 0;
+
 Scanner input = new Scanner(System.in);
 
-
-    System.out.println(question1);
-     String answer1 = input.nextLine();
-        if (answer1.equals(cAnswer1)) {
-            System.out.println("Correct"); 
-            correctAnswer++;
-        } else {
-            System.out.println("Incorrect");
+System.out.println("Hello, and welcome to all about otters. Select which fact you would like to view first (1-5)");
+     String otter1 = input.nextLine();
+        if (otter1.equals("1")) {
+            System.out.println("there are 13 extant species of otter"); 
+        
+        }
+     String otter2 = input.nextLine();
+        if (otter2.equals("2")) {
+            System.out.println("all otters are aquatic, though the sea otter is the only one that lives it's entire life in the water"); 
+        }
+     String otter3 = input.nextLine();
+        if (otter3.equals("3")) {
+            System.out.println("an otter's fur is slick and smooth, which coimbined with natural oils from it's skin makes it able to glide when sliding on it's stomach"); 
+        }
+     String otter4 = input.nextLine();
+        if (otter4.equals("4")) {
+            System.out.println("sea otters hold paws in large groups known as rafts. to prevent their young from floating away, they wrap them in kelp growing from the seafloor"); 
+        }
+     String otter5 = input.nextLine();
+        if (otter5.equals("5")) {
+            System.out.println("an otter is a keystone species, as they keep fish and urchin populations under control"); 
         }
 
-     System.out.println(question2);
-     String answer2 = input.nextLine();
-        if (answer2.equals(cAnswer2)) {
-            System.out.println("Correct"); 
-            correctAnswer++;
-        } else {
-            System.out.println("Incorrect");
-        }
-
-     System.out.println(question3);
-     String answer3 = input.nextLine();
-        if (answer3.equals(cAnswer3)) {
-            System.out.println("Correct"); 
-            correctAnswer++;
-        } else {
-            System.out.println("Incorrect");
-        }
-
-     System.out.println(question4);
-     String answer4 = input.nextLine();
-        if (answer4.equals(cAnswer4)) {
-            System.out.println("Correct"); 
-            correctAnswer++;
-        } else {
-            System.out.println("Incorrect");
-        }
-
-     System.out.println(question5);
-     String answer5 = input.nextLine();
-        if (answer5.equals(cAnswer5)) {
-            System.out.println("Correct"); 
-            correctAnswer++;
-        } else {
-            System.out.println("Incorrect");
-        }
-
-    percentage = correctAnswer * 20;
-    System.out.println("you have "+ correctAnswer +" out of 5 correct " + percentage + "%" );
+    System.out.println("you have looked through all about otters" );
     }
     
 }
