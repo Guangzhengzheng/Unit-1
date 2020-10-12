b989ca08c0ef63e60f1dd316730b5d61fedf35f0
Guangzhengzheng <819380@mystma.org>
2020-10-12 17:39:08 +0000

    Challenge 2 completed

--- unit 1/src/app/App.java
+++ unit 1/src/app/App.java
@@ -4,11 +4,28 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int count = 0;
+      Scanner input = new Scanner(System.in);
 
-        for (int i = 0; i <= 1000; i = i + 1){
-            System.out.println("The count is " + i);
+System.out.println("Top secret message available to view. You have 3 attempts at the access code and then that is it.");
+     String otter1 = input.nextLine();
+        if (otter1.equals("Swordfish")) {
+            System.out.println("Piece together the acronym of the Fully Automated Rapid Transit System and try not to laugh."); 
+        } else{
+            System.out.println("Wrong access code; please retry");
         }
-    
-}
+     String otter2 = input.nextLine();
+        if (otter2.equals("Swordfish")) {
+            System.out.println("Piece together the acronym of the Fully Automated Rapid Transit System and try not to laugh."); 
+        } else{
+            System.out.println("Wrong access code; please retry");
+        }
+     String otter3 = input.nextLine();
+        if (otter3.equals("Swordfish")) {
+            System.out.println("Piece together the acronym of the Fully Automated Rapid Transit System and try not to laugh."); 
+        } else{
+            System.out.println("Wrong access code; please retry");
+        }
+
+    System.out.println("Message recieved" );
+    }
 }
