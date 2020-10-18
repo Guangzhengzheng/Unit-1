64f3f3233abb58d7500a2d830abfe9d86dcecea6
Guangzhengzheng <819380@mystma.org>
2020-10-18 15:48:15 +0000

    Challenge 3 (backup push)

--- unit 1/src/app/App.java
+++ unit 1/src/app/App.java
@@ -6,26 +6,410 @@ public class App {
     public static void main(String[] args) throws Exception {
       Scanner input = new Scanner(System.in);
 
-System.out.println("Top secret message available to view. You have 3 attempts at the access code and then that is it.");
+System.out.println("Guess a number between 0-100");
      String otter1 = input.nextLine();
-        if (otter1.equals("Swordfish")) {
-            System.out.println("Piece together the acronym of the Fully Automated Rapid Transit System and try not to laugh."); 
-        } else{
-            System.out.println("Wrong access code; please retry");
+        if (otter1.equals("0")) {
+            System.out.println("high"); 
         }
      String otter2 = input.nextLine();
-        if (otter2.equals("Swordfish")) {
-            System.out.println("Piece together the acronym of the Fully Automated Rapid Transit System and try not to laugh."); 
-        } else{
-            System.out.println("Wrong access code; please retry");
+        if (otter2.equals("1")) {
+            System.out.println("high"); 
         }
      String otter3 = input.nextLine();
-        if (otter3.equals("Swordfish")) {
-            System.out.println("Piece together the acronym of the Fully Automated Rapid Transit System and try not to laugh."); 
-        } else{
-            System.out.println("Wrong access code; please retry");
+        if (otter3.equals("2")) {
+            System.out.println("high"); 
         }
-
-    System.out.println("Message recieved" );
-    }
+     String otter4 = input.nextLine();
+        if (otter4.equals("3")) {
+            System.out.println("high"); 
+        }
+     String otter5 = input.nextLine();
+        if (otter5.equals("4")) {
+            System.out.println("high"); 
+        }
+     String otter6 = input.nextLine();
+        if (otter6.equals("5")) {
+            System.out.println("high"); 
+        }
+     String otter7 = input.nextLine();
+        if (otter7.equals("6")) {
+            System.out.println("high"); 
+        }
+     String otter8 = input.nextLine();
+        if (otter8.equals("7")) {
+            System.out.println("high"); 
+        }
+     String otter9 = input.nextLine();
+        if (otter9.equals("8")) {
+            System.out.println("high"); 
+        }
+     String otter10 = input.nextLine();
+        if (otter10.equals("9")) {
+            System.out.println("high"); 
+        }
+     String otter11 = input.nextLine();
+        if (otter11.equals("10")) {
+            System.out.println("high"); 
+        }
+     String otter12 = input.nextLine();
+        if (otter12.equals("11")) {
+            System.out.println("high"); 
+        }
+     String otter13 = input.nextLine();
+        if (otter13.equals("12")) {
+            System.out.println("high"); 
+        }
+     String otter14 = input.nextLine();
+        if (otter14.equals("13")) {
+            System.out.println("high"); 
+        }
+     String otter15 = input.nextLine();
+        if (otter15.equals("14")) {
+            System.out.println("high"); 
+        }
+     String otter16 = input.nextLine();
+        if (otter16.equals("15")) {
+            System.out.println("high"); 
+        }
+     String otter17 = input.nextLine();
+        if (otter17.equals("16")) {
+            System.out.println("high"); 
+        }
+     String otter18 = input.nextLine();
+        if (otter18.equals("17")) {
+            System.out.println("high"); 
+        }
+     String otter19 = input.nextLine();
+        if (otter19.equals("18")) {
+            System.out.println("high"); 
+        }
+     String otter20 = input.nextLine();
+        if (otter20.equals("19")) {
+            System.out.println("high"); 
+        }
+     String otter21 = input.nextLine();
+        if (otter21.equals("20")) {
+            System.out.println("high"); 
+        }
+     String otter22 = input.nextLine();
+        if (otter22.equals("21")) {
+            System.out.println("high"); 
+        }
+     String otter23 = input.nextLine();
+        if (otter23.equals("22")) {
+            System.out.println("high"); 
+        }
+     String otter24 = input.nextLine();
+        if (otter24.equals("23")) {
+            System.out.println("high"); 
+        }
+     String otter25 = input.nextLine();
+        if (otter25.equals("24")) {
+            System.out.println("high"); 
+        }
+     String otter26 = input.nextLine();
+        if (otter26.equals("25")) {
+            System.out.println("high"); 
+        }
+     String otter27 = input.nextLine();
+        if (otter27.equals("26")) {
+            System.out.println("high"); 
+        }
+     String otter28 = input.nextLine();
+        if (otter28.equals("27")) {
+            System.out.println("high"); 
+        }
+     String otter29 = input.nextLine();
+        if (otter29.equals("28")) {
+            System.out.println("high"); 
+        }
+     String otter30 = input.nextLine();
+        if (otter30.equals("29")) {
+            System.out.println("high"); 
+        }
+     String otter31 = input.nextLine();
+        if (otter31.equals("30")) {
+            System.out.println("high"); 
+        }
+     String otter32 = input.nextLine();
+        if (otter32.equals("31")) {
+            System.out.println("high"); 
+        }
+     String otter33 = input.nextLine();
+        if (otter33.equals("32")) {
+            System.out.println("high"); 
+        }
+     String otter34 = input.nextLine();
+        if (otter34.equals("33")) {
+            System.out.println("high"); 
+        }
+     String otter35 = input.nextLine();
+        if (otter35.equals("34")) {
+            System.out.println("high"); 
+        }
+     String otter36 = input.nextLine();
+        if (otter36.equals("35")) {
+            System.out.println("high"); 
+        }
+     String otter37 = input.nextLine();
+        if (otter37.equals("36")) {
+            System.out.println("high"); 
+        }
+     String otter38 = input.nextLine();
+        if (otter38.equals("37")) {
+            System.out.println("high"); 
+        }
+     String otter39 = input.nextLine();
+        if (otter39.equals("38")) {
+            System.out.println("high"); 
+        }
+     String otter40 = input.nextLine();
+        if (otter40.equals("39")) {
+            System.out.println("high"); 
+        }
+     String otter41 = input.nextLine();
+        if (otter41.equals("40")) {
+            System.out.println("high"); 
+        }
+     String otter42 = input.nextLine();
+        if (otter42.equals("41")) {
+            System.out.println("high"); 
+        }
+     String otter43 = input.nextLine();
+        if (otter43.equals("42")) {
+            System.out.println("high"); 
+        }
+     String otter44 = input.nextLine();
+        if (otter44.equals("43")) {
+            System.out.println("high"); 
+        }
+     String otter45 = input.nextLine();
+        if (otter45.equals("44")) {
+            System.out.println("high"); 
+        }
+     String otter46 = input.nextLine();
+        if (otter46.equals("45")) {
+            System.out.println("high"); 
+        }
+     String otter47 = input.nextLine();
+        if (otter47.equals("46")) {
+            System.out.println("high"); 
+        }
+     String otter48 = input.nextLine();
+        if (otter48.equals("47")) {
+            System.out.println("high"); 
+        }
+     String otter49 = input.nextLine();
+        if (otter49.equals("48")) {
+            System.out.println("high"); 
+        }
+     String otter50 = input.nextLine();
+        if (otter50.equals("49")) {
+            System.out.println("high"); 
+        }
+     String otter51 = input.nextLine();
+        if (otter51.equals("50")) {
+            System.out.println("high"); 
+        }
+     String otter52 = input.nextLine();
+        if (otter52.equals("51")) {
+            System.out.println("high"); 
+        }
+     String otter53 = input.nextLine();
+        if (otter53.equals("52")) {
+            System.out.println("high"); 
+        }
+     String otter54 = input.nextLine();
+        if (otter54.equals("53")) {
+            System.out.println("high"); 
+        }
+     String otter55 = input.nextLine();
+        if (otter55.equals("54")) {
+            System.out.println("high"); 
+        }
+     String otter56 = input.nextLine();
+        if (otter56.equals("55")) {
+            System.out.println("high"); 
+        }
+     String otter57 = input.nextLine();
+        if (otter57.equals("56")) {
+            System.out.println("high"); 
+        }
+     String otter58 = input.nextLine();
+        if (otter58.equals("57")) {
+            System.out.println("high"); 
+        }
+     String otter59 = input.nextLine();
+        if (otter59.equals("58")) {
+            System.out.println("high"); 
+        }
+     String otter60 = input.nextLine();
+        if (otter60.equals("59")) {
+            System.out.println("high"); 
+        }
+     String otter61 = input.nextLine();
+        if (otter61.equals("60")) {
+            System.out.println("high"); 
+        }
+     String otter62 = input.nextLine();
+        if (otter62.equals("61")) {
+            System.out.println("high"); 
+        }
+     String otter63 = input.nextLine();
+        if (otter63.equals("62")) {
+            System.out.println("high"); 
+        }
+     String otter64 = input.nextLine();
+        if (otter64.equals("63")) {
+            System.out.println("high"); 
+        }
+     String otter65 = input.nextLine();
+        if (otter65.equals("64")) {
+            System.out.println("high"); 
+        }
+     String otter66 = input.nextLine();
+        if (otter66.equals("65")) {
+            System.out.println("high"); 
+        }
+     String otter67 = input.nextLine();
+        if (otter67.equals("66")) {
+            System.out.println("high"); 
+        }
+     String otter68 = input.nextLine();
+        if (otter68.equals("67")) {
+            System.out.println("high"); 
+        }
+     String otter69 = input.nextLine();
+        if (otter69.equals("68")) {
+            System.out.println("high"); 
+        }
+     String otter70 = input.nextLine();
+        if (otter70.equals("69")) {
+            System.out.println("high"); 
+        }
+     String otter71 = input.nextLine();
+        if (otter71.equals("70")) {
+            System.out.println("correct!"); 
+        }
+     String otter72 = input.nextLine();
+        if (otter72.equals("71")) {
+            System.out.println("low"); 
+        }
+     String otter73 = input.nextLine();
+        if (otter73.equals("72")) {
+            System.out.println("low"); 
+        }
+     String otter74 = input.nextLine();
+        if (otter74.equals("73")) {
+            System.out.println("low"); 
+        }
+     String otter75 = input.nextLine();
+        if (otter75.equals("74")) {
+            System.out.println("low"); 
+        }
+     String otter76 = input.nextLine();
+        if (otter76.equals("75")) {
+            System.out.println("low"); 
+        }
+     String otter77 = input.nextLine();
+        if (otter77.equals("76")) {
+            System.out.println("low"); 
+        }
+     String otter78 = input.nextLine();
+        if (otter78.equals("77")) {
+            System.out.println("low"); 
+        }
+     String otter79 = input.nextLine();
+        if (otter79.equals("78")) {
+            System.out.println("low"); 
+        }
+     String otter80 = input.nextLine();
+        if (otter80.equals("79")) {
+            System.out.println("low"); 
+        }
+     String otter81 = input.nextLine();
+        if (otter81.equals("80")) {
+            System.out.println("low"); 
+        }
+     String otter82 = input.nextLine();
+        if (otter82.equals("81")) {
+            System.out.println("low"); 
+        }
+     String otter83 = input.nextLine();
+        if (otter83.equals("82")) {
+            System.out.println("low"); 
+        }
+     String otter84 = input.nextLine();
+        if (otter84.equals("83")) {
+            System.out.println("low"); 
+        }
+     String cow85 = input.nextLine();
+        if (cow85.equals("84")) {
+            System.out.println("low"); 
+        }
+     String otter86 = input.nextLine();
+        if (otter86.equals("85")) {
+            System.out.println("low"); 
+        }
+     String otter87 = input.nextLine();
+        if (otter87.equals("86")) {
+            System.out.println("low"); 
+        }
+     String otter88 = input.nextLine();
+        if (otter88.equals("87")) {
+            System.out.println("low"); 
+        }
+     String otter89 = input.nextLine();
+        if (otter89.equals("88")) {
+            System.out.println("low"); 
+        }
+     String cow = input.nextLine();
+        if (cow.equals("89")) {
+            System.out.println("low"); 
+        }
+     String cow1 = input.nextLine();
+        if (cow1.equals("90")) {
+            System.out.println("low"); 
+        }
+     String cow2 = input.nextLine();
+        if (cow2.equals("91")) {
+            System.out.println("low"); 
+        }
+     String cow3 = input.nextLine();
+        if (cow3.equals("92")) {
+            System.out.println("low"); 
+        }
+     String cow4 = input.nextLine();
+        if (cow4.equals("93")) {
+            System.out.println("low"); 
+        }
+     String cow5 = input.nextLine();
+        if (cow5.equals("94")) {
+            System.out.println("low"); 
+        }
+     String cow6 = input.nextLine();
+        if (cow6.equals("95")) {
+            System.out.println("low"); 
+        }
+     String cow7 = input.nextLine();
+        if (cow7.equals("96")) {
+            System.out.println("low"); 
+        }
+     String cow8 = input.nextLine();
+        if (cow8.equals("97")) {
+            System.out.println("low"); 
+        }
+     String cow9 = input.nextLine();
+        if (cow9.equals("98")) {
+            System.out.println("low"); 
+        }
+     String cow10 = input.nextLine();
+        if (cow10.equals("99")) {
+            System.out.println("low"); 
+        }
+     String cow11 = input.nextLine();
+        if (cow11.equals("100")) {
+            System.out.println("low");
+    } 
+}
 }
